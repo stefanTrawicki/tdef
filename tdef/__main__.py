ef7c88e0aa62548664e9961698dec01e6e31736b
Stefan Trawicki <s.trawicki@me.com>
2023-07-22 10:33:04 +0000

    Updated structure
    driver.py -> Reads json file containing jobs, either calls TDEF class directly or invokes it with a command, i.e for use with NVidia NCU. Still working out how to read subproccess output reliably.
    
    make_job.py and job_description -> job_description.json is the evaluation matrix of  jobs to run, and make job converts this into an array of  jobs in jobs.json. This could of course have been done elsewhere but I like having an exact set of instructions written in json for sanity checks and easy replayability.
    
    tdef/__main__.py -> basically adjusted to allow json from the command line. Can also pass --notune flag to force no tuning and simply test the tvm compiled model.
    
    tdef/src/tdef.py -> changed to use jsonschema package to define and validate jobs, and kind of hackily apply the defaults to the passed job json if it is missing keys. Also changed how the hashing works; a dir is made for the model being run (defined in job), and a directory named after the hash of the job inside that, then metadata, records and timing information made inside that. Model also does 10 shakedown inferences before running for the profiled run. Finally some of the logic for setting the input name and size is now done in tdef instead of the inferRandom method of tvm_model, because its easier given access to keys.

--- tdef/__main__.py
+++ tdef/__main__.py
@@ -4,37 +4,36 @@ from .src.dump_model import dumpONNX, dumpPyTorch
 import argparse
 import json
 
-def process(json_data):
-    for trial in json_data["trials"]:
-        for model, model_data in json_data["model"].items():
-            for autoschedule in json_data["autoschedule"]:
-                job = {
-                    "trials": trial,
-                    "gpus": [0, 1],
-                    "enable_autoscheduler": autoschedule,
-                    "path": model_data["path"],
-                    "input_name": model_data["input_name"],
-                    "input_shape": model_data["input_shape"],
-                    "override_records": json_data.get("override_records", None)
-                }
-                if not autoschedule:
-                    for tuner in json_data["tuner"]:
-                        job["tuner"] = tuner
-                        print(job)
-                        TDEF(job=job, dry_run=False, sample=True)
-                else:
-                    print(job)
-                    TDEF(job=job, dry_run=False, sample=True)
+example = """
+    {
+        "trials": 50,
+        "tuner": "random",
+        "model": "resnet18",
+        "path": "dumps/onnx/frozen/resnet18-v1-7.onnx",
+        "input_name": "data",
+        "input_shape": [1, 3, 224, 224]
+    }
+    """
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='Input job')
-    parser.add_argument('json_file', help='Path to the JSON file')
-    return parser.parse_args()
+# python -m tdef --json_string <this>
+example_as_str = '{"trials": 50,"tuner": "random","model": "resnet18","path": "dumps/onnx/frozen/resnet18-v1-7.onnx","input_name": "data","input_shape": [1, 3, 224, 224]}'
 
-if __name__ == '__main__':
-    args = parse_arguments()
+def main(data:dict, force_notune=False):
+    return TDEF(job_json=data, force_no_tuning=force_notune)
 
-    with open(args.json_file) as f:
-        json_data = json.load(f)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Parse JSON string from command line.")
+    parser.add_argument("--json_string", type=str, help="JSON string to be parsed", required=True)
+    parser.add_argument("--notune", action="store_true", help="Whether to force disable tuning (run baseline)", default=False)
+    args = parser.parse_args()
+    
+    json_string = args.json_string
+    
+    try:
+        data = json.loads(json_string)
+    except json.JSONDecodeError as e:
+        print("Error: Invalid JSON format.")
+        print(e)
+        exit
 
-    process(json_data)
+    print(f"output_dir: {main(data, args.notune)}")
--- tdef/src/tdef.py
+++ tdef/src/tdef.py
@@ -3,10 +3,10 @@ import json
 import numpy as np
 import hashlib
 from tqdm import trange
+import jsonschema
 
 # ============== TDEF imports ==============
 from .tvm_model import Model
-
 from .utilities import DefaultLog, ConfigureLog
 
 def get_hash(data:dict) -> str:
@@ -17,110 +17,151 @@ def get_hash(data:dict) -> str:
     s = "_".join(s)
     return hashlib.sha1(s.encode("utf-8")).hexdigest()
 
+job_schema = {
+    "type": "object",
+    "properties": {
+        "model": {"type": "string", "default": None},
+        "path": {"type": "string", "default": None},
+        "records": {"type": "string", "default": None},
+        "input_shape": {
+            "type": "array",
+            "items": {"type": "integer"},
+            "default": [1, 3, 224, 224],
+        },
+        "input_name": {"type": "string", "default": "data"},
+        "enable_autoscheduler": {"type": "boolean", "default": False},
+        "parallel": {"type": "integer", "default": 4},
+        "trials": {"type": "integer", "default": None},
+        "timeout": {"type": "integer", "default": 100},
+        "repeat": {"type": "integer", "default": 1},
+        "number": {"type": "integer", "default": 10},
+        "mixed_precision": {"type": "boolean", "default": False},
+        "opt_level": {"type": "integer", "default": 3},
+        "gpus": {
+            "type": "array",
+            "items": {"type": "integer"},
+            "default": [0, 1],
+        },
+        "target": {"type": "string", "default": "cuda -arch=sm_80"},
+        "min_repeat_ms": {"type": "integer", "default": 1000},
+        "tuner": {"type": "string", "default": "xgb_rank"},
+        "early_stopping": {"type": "integer", "default": 1000},
+        "include_simple_tasks": {"type": "boolean", "default": False},
+        "hardware_params_num_cores": {"type": "integer", "default": None},
+        "hardware_params_vector_unit_bytes": {"type": "integer", "default": None},
+        "hardware_params_cache_line_bytes": {"type": "integer", "default": None},
+        "hardware_params_max_shared_memory_per_block": {"type": "integer", "default": None},
+        "hardware_params_max_local_memory_per_block": {"type": "integer", "default": None},
+        "hardware_params_max_threads_per_block": {"type": "integer", "default": None},
+        "hardware_params_max_vthread_extent": {"type": "integer", "default": None},
+        "hardware_params_warp_size": {"type": "integer", "default": None},
+        "hardware_params_target": {"type": "string", "default": "cuda -arch=sm_80"},
+        "hardware_params_target_host": {"type": "string", "default": None},
+    },
+}
+
+def pretty_print_schema(schema):
+    schema_json_str = json.dumps(schema, indent=2, sort_keys=True)
+    schema_dict = json.loads(schema_json_str)
+    max_key_length = max(len(key) for key in schema_dict.keys())
+    for key, value in schema_dict.items():
+        padding = max_key_length - len(key) + 2
+        print(f"{key}{' ' * padding}: {value}")
+
+def assign_defaults(data, schema):
+    merged_data = data.copy()
+    for key, field in schema.get("properties", {}).items():
+        if key not in merged_data:
+            merged_data[key] = field.get("default")
+    merged_data["hardware_params_target"] = merged_data.get("target")
+    return merged_data
+
+def create_hashed_dir(data_dict):
+    json_string = json.dumps(data_dict, sort_keys=True)
+    hash_value = hashlib.sha256(json_string.encode()).hexdigest()
+    model_dir = f"records/{data_dict.get('model', 'default_model')}/{hash_value}/"
+    metadata_filename = os.path.join(model_dir, f"metadata.json")
+    records_filename = os.path.join(model_dir, f"records.json")
+    already_existed = os.path.exists(records_filename) and os.path.isfile(records_filename)
+    if already_existed:
+        already_existed = os.path.getsize(records_filename) > 10
+    os.makedirs(model_dir, exist_ok=True)
+    with open(metadata_filename, 'w') as file:
+        file.write(json.dumps(data_dict, indent=2))
+    with open(records_filename, "w+") as file:
+        pass
+    return model_dir, already_existed
+
+def run_model_x_times(model, x, profile=False):
+        times = []
+        for i in trange(x):
+            t, o = model.inferRandom(profile=profile)
+            times.append(t)
+        avg = sum(times) / len(times)
+        times.sort()
+        min_t = times[0]
+        max_t = times[-1]
+        med_t = times[int(len(times)/2)]
+        var = np.var(times)
+        info = {
+            "timing": {
+                "min": min_t,
+                "max": max_t,
+                "med": med_t,
+                "var": var,
+                "avg": avg,
+                "times": times
+            }
+        }
+        return info
 
 class TDEF():
-    def records_and_data(self, job:dict) -> str:
-        hash_code = get_hash(job)
-        records = f"records/{hash_code}_records.json"
-        data = f"records/{hash_code}_data.json"
-        job["hash"] = hash_code
+    def __init__(self, job_json, force_no_tuning=False):
+
+        try:
+            jsonschema.validate(instance=job_json, schema=job_schema)
+            job = assign_defaults(job_json, job_schema)
+        except jsonschema.exceptions.ValidationError as e:
+            print("Job was invalid:")
+            print(e.messages)
+            return
+        
+        pretty_print_schema(job)
+        dir, test_ran_previously = create_hashed_dir(job)
+        records = os.path.join(dir, "records.json")
         job["records"] = records
-        records_exist = os.path.exists(records)
-        if not records_exist:
-            with open(data, "w+") as f:
-                f.write(json.dumps(job, indent=4))
-        return records_exist, records
-
-    def __init__(self, job:dict = {}, dry_run=False, sample=False):
-
-        records_override = job["override_records"]
-        del(job["override_records"])
-
-        job["input_shape"] = job.get("input_shape", (1, 3, 224, 224))
-        job["input_name"] = job.get("input_name", "data")
-        job["enable_autoscheduler"] = job.get("enable_autoscheduler", False)
-        job["parallel"] = job.get("parallel", 4)
-        job["trials"] = job.get("trials", None)
-        job["timeout"] = job.get("timeout", 100)
-        job["repeat"] = job.get("repeat", 1)
-        job["number"] = job.get("number", 10)
-        job["mixed_precision"] = job.get("mixed_precision", False)
-        job["opt_level"] = job.get("opt_level", 3) 
-        job["gpus"] = job.get("gpus", [0, 1])
-        job["target"] = job.get("target", "cuda -arch=sm_80")
-        job["min_repeat_ms"] = job.get("min_repeat_ms", 1000)
-        job["tuner"] = job.get("tuner", "xgb")
-        job["early_stopping"] = job.get("early_stopping", 1000)
-        job["include_simple_tasks"] = job.get("include_simple_tasks", False)
-
-        job["hardware_params_num_cores"] = job.get("hardware_params_num_cores", None)
-        job["hardware_params_vector_unit_bytes"] = job.get("hardware_params_vector_unit_bytes", None)
-        job["hardware_params_cache_line_bytes"] = job.get("hardware_params_cache_line_bytes", None)
-        job["hardware_params_max_shared_memory_per_block"] = job.get("hardware_params_max_shared_memory_per_block", None)
-        job["hardware_params_max_local_memory_per_block"] = job.get("hardware_params_max_local_memory_per_block", None)
-        job["hardware_params_max_threads_per_block"] = job.get("hardware_params_max_threads_per_block", None)
-        job["hardware_params_max_vthread_extent"] = job.get("hardware_params_max_vthread_extent", None)
-        job["hardware_params_warp_size"] = job.get("hardware_params_warp_size", None)
-        job["hardware_params_target"] = job.get("hardware_params_target", job["target"])
-        job["hardware_params_target_host"] = job.get("hardware_params_target_host", None)
-
-        records_exist, records_path = self.records_and_data(job)
-        job["records"] = records_path
 
         model = None
-        if not records_exist:
-            print(f"Records {records_path!a} not found, will tune")
-            if records_override is not None:
-                print('will not tune, records overridden!')
-                job["records"] = records_override
-                if sample:
-                    model = Model(job)
-                    model.compile(job)
-                    t, o = model.inferRandom(job=job, profile=True)
-                    print(f"{t}")
-                    exit
+        if not force_no_tuning:
+            if not test_ran_previously:
+                print(f"First time combination ({dir!a}) ran, beginning tuning")
+                model = Model(job)
+                model.tune(job)
+            else:
+                print(f"Combination ({dir!a}) ran previously, no tuning required")
 
-            model = Model(job)
-            model.tune(job, dry_run=dry_run)
+        if force_no_tuning:
+            print("Will not use any tuning records, baseline TVM compiled run (will still apply selected graph level opt)")
+
+        model = Model(job)
+        model.compile(job)
+        model.module.set_input(job["input_name"], np.random.rand(*job["input_shape"]))
+
+        # see if this has a bearing on perf, the earliest runs can be a bit slow
+        x = 10
+        print(f"Running {x} shakedown inferences...")
+        timing = run_model_x_times(model, x, profile=False)
+        print("Running measured inference...")
+        run_model_x_times(model, 1, profile=True)
+
+        if not force_no_tuning:
+            with open(os.path.join(dir, "timings.json"), "w+") as f:
+                f.write(json.dumps(timing, indent=2))
         else:
-            print(f"Existing records {records_path!a} found, tuning not required")
-            if sample:
-                model = Model(job)
-                model.compile(job)
-                t, o = model.inferRandom(job=job, profile=True)
-                print(f"{t}")
+            with open(os.path.join(dir, "timings_notune.json"), "w+") as f:
+                f.write(json.dumps(timing, indent=2))
 
-        if not dry_run:
-            if not sample:
-                model = Model(job)
-                model.compile(job)
-                times = []
-                outs = []
-                for i in trange(100):
-                    t, o = model.inferRandom(job=job, profile=True)
-                    times.append(t)
-                    outs.append(o)
-                avg = sum(times) / len(times)
-                times.sort()
-                min_t = times[0]
-                max_t = times[-1]
-                med_t = times[int(len(times)/2)]
-                var = np.var(times)
-                print(f"avg {avg} ms \n" +
-                    f"min {min_t} ms \n" +
-                    f"med {med_t} ms \n" +
-                    f"max {max_t} ms \n" +
-                    f"variance {var}")
-                info = {
-                    "timing": {
-                        "hash": job["hash"],
-                        "min": min_t,
-                        "max": max_t,
-                        "med": med_t,
-                        "var": var,
-                        "avg": avg,
-                        "times": times
-                    }
-                }
-                with open(f"records/{job['hash']}_outputs.json", "w+") as out:
-                    out.write(json.dumps(info, indent=4))
+        self.dir = dir
+
+    def __str__(self):
+        return self.dir
